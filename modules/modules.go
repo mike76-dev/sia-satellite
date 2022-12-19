0bf448ab5afdaf3e2aa1ea62610b5f701b8e7fb3
mike76-dev <michael.bulanov@yahoo.com>
2022-12-19 16:55:45 +0100

    Add methods to Manager interface

--- modules/modules.go
+++ modules/modules.go
@@ -53,9 +53,37 @@ type Satellite interface {
 // hosts.
 type Manager interface {
 	smodules.Alerter
-	
+
+	// ActiveHosts provides the list of hosts that the manager is selecting,
+	// sorted by preference.
+	ActiveHosts() ([]smodules.HostDBEntry, error)
+
+	// AllHosts returns the full list of hosts known to the manager.
+	AllHosts() ([]smodules.HostDBEntry, error)
+
 	// Close safely shuts down the manager.
 	Close() error
+
+	// EstimateHostScore will return the score for a host with the provided
+	// settings, assuming perfect age and uptime adjustments.
+	EstimateHostScore(entry smodules.HostDBEntry, allowance smodules.Allowance) (smodules.HostScoreBreakdown, error)
+
+	// Filter returns the hostdb's filterMode and filteredHosts.
+	Filter() (smodules.FilterMode, map[string]types.SiaPublicKey, []string, error)
+
+	// SetFilterMode sets the hostdb's filter mode.
+	SetFilterMode(smodules.FilterMode, []types.SiaPublicKey, []string) error
+
+	// Host provides the DB entry and score breakdown for the requested host.
+	Host(pk types.SiaPublicKey) (smodules.HostDBEntry, bool, error)
+
+	// InitialScanComplete returns a boolean indicating if the initial scan of
+	// the hostdb is completed.
+	InitialScanComplete() (bool, error)
+
+	// ScoreBreakdown will return the score for a host db entry using the
+	// hostdb's weighting algorithm.
+	ScoreBreakdown(entry smodules.HostDBEntry) (smodules.HostScoreBreakdown, error)
 }
 
 // Provider implements the methods necessary to communicate with the
