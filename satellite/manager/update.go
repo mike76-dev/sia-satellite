97463c31dc9806a9db8b72047513e69d492e86cf
mike76-dev <michael.bulanov@yahoo.com>
2023-04-13 12:18:32 +0200

    Cap network averages

--- satellite/manager/update.go
+++ satellite/manager/update.go
@@ -5,12 +5,50 @@ import (
 
 	"github.com/mike76-dev/sia-satellite/modules"
 
+	smodules "go.sia.tech/siad/modules"
 	"go.sia.tech/siad/types"
 )
 
 const (
 	calculateAveragesFrequency = 10 * time.Minute
 )
+
+var (
+	// Some sane values to cap the averages.
+	saneStoragePrice = types.SiacoinPrecision.Mul64(1e4)  // 10KS
+	saneCollateral = types.SiacoinPrecision.Mul64(2e4)    // 20KS
+	saneUploadPrice = types.SiacoinPrecision.Mul64(1e4)   // 10KS
+	saneDownloadPrice = types.SiacoinPrecision.Mul64(1e5) // 100KS
+	saneContractPrice = types.SiacoinPrecision.Mul64(100) // 100SC
+	saneBaseRPCPrice = types.SiacoinPrecision             // 1SC
+	saneSectorAccessPrice = types.SiacoinPrecision        // 1SC
+)
+
+// capAverages checks if the host settings exceed the sane values.
+func capAverages(entry smodules.HostDBEntry) bool {
+	if entry.StoragePrice.Cmp(saneStoragePrice) > 0 {
+		return true
+	}
+	if entry.Collateral.Cmp(saneCollateral) > 0 {
+		return true
+	}
+	if entry.UploadBandwidthPrice.Cmp(saneUploadPrice) > 0 {
+		return true
+	}
+	if entry.DownloadBandwidthPrice.Cmp(saneDownloadPrice) > 0 {
+		return true
+	}
+	if entry.ContractPrice.Cmp(saneContractPrice) > 0 {
+		return true
+	}
+	if entry.BaseRPCPrice.Cmp(saneBaseRPCPrice) > 0 {
+		return true
+	}
+	if entry.SectorAccessPrice.Cmp(saneSectorAccessPrice) > 0 {
+		return true
+	}
+	return false
+}
 
 // calculateAverages calculates the host network averages from HostDB.
 func (m *Manager) calculateAverages() {
@@ -35,8 +73,12 @@ func (m *Manager) calculateAverages() {
 	}
 
 	// Sum up the values.
+	var numHosts uint64
 	for _, entry := range hosts {
-		m.hostAverages.Duration = types.BlockHeight(int64(m.hostAverages.Duration) + int64(entry.MaxDuration))
+		if capAverages(entry) {
+			continue
+		}
+		m.hostAverages.Duration = types.BlockHeight(uint64(m.hostAverages.Duration) + uint64(entry.MaxDuration))
 		m.hostAverages.StoragePrice = m.hostAverages.StoragePrice.Add(entry.StoragePrice)
 		m.hostAverages.Collateral = m.hostAverages.Collateral.Add(entry.Collateral)
 		m.hostAverages.DownloadBandwidthPrice = m.hostAverages.DownloadBandwidthPrice.Add(entry.DownloadBandwidthPrice)
@@ -44,18 +86,25 @@ func (m *Manager) calculateAverages() {
 		m.hostAverages.ContractPrice = m.hostAverages.ContractPrice.Add(entry.ContractPrice)
 		m.hostAverages.BaseRPCPrice = m.hostAverages.BaseRPCPrice.Add(entry.BaseRPCPrice)
 		m.hostAverages.SectorAccessPrice = m.hostAverages.SectorAccessPrice.Add(entry.SectorAccessPrice)
+		numHosts++
 	}
 
-	// Divide by the number of active hosts.
-	m.hostAverages.NumHosts = uint64(len(hosts))
-	m.hostAverages.Duration = types.BlockHeight(int(m.hostAverages.Duration) / len(hosts))
-	m.hostAverages.StoragePrice = m.hostAverages.StoragePrice.Div64(uint64(len(hosts)))
-	m.hostAverages.Collateral = m.hostAverages.Collateral.Div64(uint64(len(hosts)))
-	m.hostAverages.DownloadBandwidthPrice = m.hostAverages.DownloadBandwidthPrice.Div64(uint64(len(hosts)))
-	m.hostAverages.UploadBandwidthPrice = m.hostAverages.UploadBandwidthPrice.Div64(uint64(len(hosts)))
-	m.hostAverages.ContractPrice = m.hostAverages.ContractPrice.Div64(uint64(len(hosts)))
-	m.hostAverages.BaseRPCPrice = m.hostAverages.BaseRPCPrice.Div64(uint64(len(hosts)))
-	m.hostAverages.SectorAccessPrice = m.hostAverages.SectorAccessPrice.Div64(uint64(len(hosts)))
+	m.hostAverages.NumHosts = numHosts
+
+	// Zero check.
+	if numHosts == 0 {
+		return
+	}
+
+	// Divide by the number of hosts.
+	m.hostAverages.Duration = types.BlockHeight(uint64(m.hostAverages.Duration) / numHosts)
+	m.hostAverages.StoragePrice = m.hostAverages.StoragePrice.Div64(numHosts)
+	m.hostAverages.Collateral = m.hostAverages.Collateral.Div64(numHosts)
+	m.hostAverages.DownloadBandwidthPrice = m.hostAverages.DownloadBandwidthPrice.Div64(numHosts)
+	m.hostAverages.UploadBandwidthPrice = m.hostAverages.UploadBandwidthPrice.Div64(numHosts)
+	m.hostAverages.ContractPrice = m.hostAverages.ContractPrice.Div64(numHosts)
+	m.hostAverages.BaseRPCPrice = m.hostAverages.BaseRPCPrice.Div64(numHosts)
+	m.hostAverages.SectorAccessPrice = m.hostAverages.SectorAccessPrice.Div64(numHosts)
 
 	return
 }
