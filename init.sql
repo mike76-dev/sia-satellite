0ff6ab48066fd0ddce88863eb1a64f1869ee3020
mike76-dev <mike76@Michaels-MacBook-Air.local>
2023-08-21 23:21:03 +0200

    Charge a fee for file repairs

--- init.sql
+++ init.sql
@@ -497,7 +497,8 @@ CREATE TABLE ctr_metadata (
 	enc_key   BINARY(32) NOT NULL,
 	filepath  VARCHAR(255) NOT NULL UNIQUE,
 	renter_pk BINARY(32) NOT NULL,
-	uploaded  BIGINT UNSIGNED NOT NULL,
+	updated   BIGINT UNSIGNED NOT NULL,
+	retrieved BIGINT UNSIGNED NOT NULL,
 	PRIMARY KEY (enc_key),
 	FOREIGN KEY (renter_pk) REFERENCES ctr_renters(public_key)
 );
