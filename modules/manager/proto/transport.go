a5d7771affdcf5cf34557d36281881ccce2fa8ba
mike76-dev <michael.bulanov@yahoo.com>
2023-09-06 09:38:13 +0200

    Format

--- modules/manager/proto/transport.go
+++ modules/manager/proto/transport.go
@@ -15,7 +15,7 @@ func dial(ctx context.Context, hostIP string) (net.Conn, error) {
 	return conn, err
 }
 
-//WithTransportV2 creates a transport and calls an RHP2 RPC.
+// WithTransportV2 creates a transport and calls an RHP2 RPC.
 func WithTransportV2(ctx context.Context, hostIP string, hostKey types.PublicKey, fn func(*rhpv2.Transport) error) (err error) {
 	conn, err := dial(ctx, hostIP)
 	if err != nil {
@@ -43,7 +43,7 @@ func WithTransportV2(ctx context.Context, hostIP string, hostKey types.PublicKey
 	return fn(t)
 }
 
-//WithTransportV3 creates a transport and calls an RHP3 RPC.
+// WithTransportV3 creates a transport and calls an RHP3 RPC.
 func WithTransportV3(ctx context.Context, siamuxAddr string, hostKey types.PublicKey, fn func(*rhpv3.Transport) error) (err error) {
 	conn, err := dial(ctx, siamuxAddr)
 	if err != nil {
